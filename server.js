273b21799ce43e721fb84df9360fd35cef8b9933
Aman Abhishek <70442464+Aman-Abhishek-18@users.noreply.github.com>
2024-06-20 20:35:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const sql = require("mssql");
 var config = {
     server: "TEST\\SQLEXPRESS", //When specifying the server name "TEST\SQLEXPRESS" in contexts like connection 
     //strings or configurations, you need to use double backslashes (\\) 
-    database: "TestDB", // your database name
+    database: "FirstDB", // your database name
     user: "sa",  // user name 
-    password: "test123", // password
+    password: "check1234", // password
     options: {
         trustedConnection: false, // Use false if using username and password
         encrypt: false, // Use false if not connecting to Azure
@@ -19,28 +19,18 @@ sql.connect(config, function(err) {
         return;
     }
     const request = new sql.Request();
-    request.query("SELECT * FROM Animal_Identification where identificationNumber='123412341234'", function(err, result) {
+    request.query("SELECT * FROM forbes_global_2010_2014'", function(err, result) {
         if (err) console.log("Query error: ", err);
         else {
             const myJson = result.recordset;
             console.log(myJson); // JSON data
 
-            const myJsonStr = JSON.stringify(myJson);
-            console.log(myJsonStr); // JSON string
+            // const myJsonStr = JSON.stringify(myJson);
+            // console.log(myJsonStr); // JSON string
 
-            const jsonArray = JSON.parse(myJsonStr); //Parse the JSON string into a JavaScript object
-            const firstObject = jsonArray[0]; //Access the first object in the array
-            const petName = firstObject.petName;
-            const breedName = firstObject.breedName;
-            const dob = firstObject.dob;
-            const previousVaccinationDate = firstObject.previousVaccinationDate;
-            const diseaseHistory = firstObject.diseaseHistory;
-            const ownerName = firstObject.ownerName;
-            const weight = firstObject.weight;
-            const address = firstObject.address;
-            console.log('Pet Name:', petName);
-            console.log('Date of Birth:', dob);
-            console.log('Breed Name:', breedName);    
+            // const jsonArray = JSON.parse(myJsonStr); //Parse the JSON string into a JavaScript object
+            // const firstObject = jsonArray[0]; //Access the first object in the array
+
         }
     });
 });
